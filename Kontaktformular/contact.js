88bfe8eace440fe6e64bd5ec03aef1019e639649
Simon Wilmink <wilminksimon@googlemail.com>
2026-03-03 00:24:52 +0100

    Kontakt Seite hinzugefügt

---
+++ Kontaktformular/contact.js
@@ -0,0 +1,16 @@
+function sendMail(event){﻿
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("https://formspree.io/f/xpqjgrok", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./send_mail.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
